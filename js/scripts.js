008563f3fc628232df766790f4c0b386db463684
Marianna <marry-cherry-pie@mail.ru>
2018-09-08 13:19:31 +0300

    validation form & bounce

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 var feedbackLink = document.querySelector(".button-feedback");
-var popup = document.querySelector(".modal-login-container");
+var popup = document.querySelector(".feedback-overlay");
 var close = document.querySelector(".modal-feedback-close");
-var nameFieldset = popup.querySelector("[id=feedback-name]");
-var form = popup.querySelector("modal-feedback");
-var emailFieldset = popup.querySelector("[id=feedback-email]");
+var nameFieldset = popup.querySelector("[name=feedback-name]");
+var form = popup.querySelector("form");
+var emailFieldset = popup.querySelector("[name=feedback-email]");
 
 
 
@@ -17,10 +17,29 @@ nameFieldset.focus();
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
 })
-close.addEventListener("keydown", function (evt) {
-  evt.keyCode === 27 && (evt.preventDefault(),
-  popup.classList.contains("modal-show") && (popup.classList.remove("modal-show")))
+
+form.addEventListener("submit", function (evt) {
+  if (!nameFieldset.value || !emailFieldset.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+      popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+    console.log("Нужно ввести имя и email");
+  }
+})
+
+
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
+    }
+  }
 });
 
 
